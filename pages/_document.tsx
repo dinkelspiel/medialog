254b109da2bce5d715b161c27bcc23d13d30a648
shykeiichi <mail@keii.dev>
2023-01-16 19:21:34 +0100

    remove title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,9 +3,6 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Medialog</title>
-      </Head>
       <body>
         <Main />
         <NextScript />
