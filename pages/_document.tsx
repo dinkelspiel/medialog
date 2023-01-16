17377100ee656839d8006d77092226cfbadd3cf2
shykeiichi <mail@keii.dev>
2023-01-16 19:22:28 +0100

    remove title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <body>
+	<Head> </Head>
+	<body>
         <Main />
         <NextScript />
       </body>
