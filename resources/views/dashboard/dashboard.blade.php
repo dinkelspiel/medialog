186500d80ad54518e68ec4f8577d7d0b3bbf29d5
22widi <22widi@ssis.nu>
2023-09-12 12:33:59 +0200

    Updated dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -8,17 +8,19 @@
     @vite('resources/css/app.css')
 </head>
 <body>
-    <div class="grid grid-cols-[5rem,1fr,1fr,1fr] h-screen bg-color">
+    <div class="grid grid-cols-[5rem,1fr,1fr,1fr] h-screen bg-color mr-3">
         <div class="grid-item">
-            @include('includes/header')
+            @include('includes/header', [
+                'page' => 'home'
+            ])
         </div>
         <div class="grid-item my-3 rounded-lg bg-card">
-            
+            @invlude('dashboard/includes/add')
         </div>
         <div class="grid-item m-3 flex flex-col">
 
         </div>
-        <div class="grid-item my-3 mr-3 rounded-lg bg-card">
+        <div class="grid-item my-3 rounded-lg bg-card">
             
         </div>
     </div> 
