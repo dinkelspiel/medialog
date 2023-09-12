31e62e44e67a32f932eb3f9c0d1e363574110e56
keii <mail@keii.dev>
2023-09-12 13:52:03 +0200

    Update

--- resources/views/dashboard/includes/add.blade.php
+++ resources/views/dashboard/includes/add.blade.php
@@ -1,3 +1,18 @@
-<div class="">
-
+<div class="grid grid-cols-2 m-3 gap-3">
+    <input class="grid-item col-span-2 input">
+    <div class="grid-item btn">
+        a
+    </div>
+    <div class="grid-item btn">
+        b
+    </div>
+    <div class="grid-item btn">
+        b
+    </div>
+    <div class="grid-item btn">
+        b
+    </div>
+    <div class="grid-item col-span-2 btn">
+        Search
+    </div>
 </div>
