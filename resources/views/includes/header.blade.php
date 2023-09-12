1c57e64fe9d4c7370285556739ca1b0dddf25d0c
22widi <22widi@ssis.nu>
2023-09-12 12:33:59 +0200

    Updated dashboard

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col py-3 h-screen justify-center items-center gap-2 justify-start">
+<div class="flex flex-col py-3 h-screen items-center gap-2 justify-start">
     <div class="w-14 h-14 rounded-lg bg-violet-200 justify-center items-center flex cursor-pointer hover:bg-violet-300 active:bg-violet-100 transition-all duration-75">
         <img src="/assets/logo.svg">
     </div>
