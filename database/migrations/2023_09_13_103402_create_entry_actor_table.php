6b27c3d1dad759f6bccbdfd213dedea3b413f943
keii <mail@keii.dev>
2023-09-13 12:37:18 +0200

    Added last tables

--- database/migrations/2023_09_13_103402_create_entry_actor_table.php
+++ database/migrations/2023_09_13_103402_create_entry_actor_table.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Models\Franchise;
-use App\Models\Studio;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,12 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('entries', function (Blueprint $table) {
+        Schema::create('entry_actor', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Franchise::class);
-            $table->text('name');
-            $table->foreignIdFor(Studio::class);
-            $table->text('cover_url');
+            $table->foreignIdFor(Entry::class);
+            $table->foreignIdFor(Person::class);
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('entries');
+        Schema::dropIfExists('entry_actor');
     }
 };
