e1b9a4ca357737b14bdb444dc6d529517f001b56
keiidev <mail@keii.dev>
2023-09-22 21:53:23 +0200

    I'm feeling lucky implementation

--- app/Livewire/Dashboard/FilterUserEntriesBrowser.php
+++ app/Livewire/Dashboard/FilterUserEntriesBrowser.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Dashboard;
 
+use App\Models\Entry;
 use App\Models\Franchise;
 use App\Models\UserEntry;
 use Livewire\Component;
@@ -10,11 +11,44 @@ class FilterUserEntriesBrowser extends Component
 {
     public ?Franchise $franchise = null;
 
+    public bool $includeAllFranchises = false;
+    public bool $includeAlreadyWatched = false;
+
+    public function toggleIncludeAllFranchises()
+    {
+        $this->includeAllFranchises = !$this->includeAllFranchises;
+    }
+
+    public function toggleIncludeAlreadyWatched()
+    {
+        $this->includeAlreadyWatched = !$this->includeAlreadyWatched;
+    }
+
     public function getRandom()
     {
         $user = auth()->user();
-        $userEntry = UserEntry::inRandomOrder()->where('user_id', $user->id)->where('rating', null)->first();
-        $this->franchise = $userEntry->entry->franchise;
+
+        if(!$this->includeAllFranchises)
+        {
+            if($this->includeAlreadyWatched)
+            {
+                $userEntry = UserEntry::inRandomOrder()->where('user_id', $user->id)->first();
+            } else 
+            {
+                $userEntry = UserEntry::inRandomOrder()->where('user_id', $user->id)->where('rating', null)->first();
+            }
+            $this->franchise = $userEntry->entry->franchise;
+        } else {
+            if($this->includeAlreadyWatched)
+            {
+                $this->franchise = Franchise::inRandomOrder()->has('entries')->first();
+            } else 
+            {
+                $this->franchise = Entry::inRandomOrder()->whereDoesntHave('userEntries', function ($query) use ($user) {
+                    $query->where('user_id', $user->id)->where('rating', '!=', null);
+                })->first()->franchise;
+            }
+        }
     }
 
     public function render()
@@ -22,7 +56,9 @@ class FilterUserEntriesBrowser extends Component
         $user = auth()->user();
         return view('livewire.dashboard.filter-user-entries-browser', [
             'franchise' => $this->franchise,
-            'canGetRandom' => count(UserEntry::where('user_id', $user->id)->where('rating', null)->get()) >= 1
+            'canGetRandom' => count(UserEntry::where('user_id', $user->id)->where('rating', null)->get()) >= 1,
+            'includeAllFranchises' => $this->includeAllFranchises,
+            'includeAlreadyWatched' => $this->includeAlreadyWatched
         ]);
     }
 }
--- resources/views/livewire/dashboard/filter-user-entries-browser.blade.php
+++ resources/views/livewire/dashboard/filter-user-entries-browser.blade.php
@@ -12,7 +12,21 @@
     <div class="py-6 border-b w-full border-b-outline border-dashed">
         <input class="input w-full" placeholder="Sort After">
     </div>
-    <div class="pt-6 w-full">
+    <div class="flex flex-row gap-3 items-center pt-6">
+        <div class="flex flex-row gap-2">
+            <div>
+                Include all franchises
+            </div>
+            <input type="checkbox" wire:click="toggleIncludeAllFranchises" @if($includeAllFranchises) checked @endif>
+        </div>
+        <div class="flex flex-row gap-2">
+            <div>
+                Include already watched
+            </div>
+            <input type="checkbox" wire:click="toggleIncludeAlreadyWatched" @if($includeAlreadyWatched) checked @endif>
+        </div>
+    </div>
+    <div class="pt-3 w-full">
         <button class="btn" @if(!$canGetRandom)disabled @else wire:click="getRandom" @endif>
             I'm feeling lucky
         </button>
@@ -22,13 +36,12 @@
     </div>
     @if(!$canGetRandom)
         <div class="error mt-3">
-            You must have atleast one un-completed franchise to get a random one
+            You must have atleast one un-completed franchise
         </div>
     @endif
-
     @if(!is_null($franchise))
         <div class="pt-6 rounded-lg h-20 w-full object-cover flex flex-col gap-3">
-            <img src="{{ $franchise->entries->first()->cover_url }}" class="rounded-lg h-20 w-full object-cover">
+            <img src="{{ $franchise->entries->first()->cover_url }}" class="rounded-lg h-60 w-full object-cover">
             <div>
                 <i>{{ $franchise->name }}</i>. A <i>{{ $franchise->category->name }}</i> made by @foreach ($franchise->entries as $entry) <i>{{ $entry->studio->name }}</i>@if($loop->remaining == 1) and @elseif(!$loop->last),@endif @endforeach
             </div>
