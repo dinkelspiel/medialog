7ca11eda8c31a117e50e12fdd91754df3b0d6401
keii <mail@keii.dev>
2023-09-22 11:27:32 +0200

    Simple i'm feeling lucky

--- app/Livewire/Dashboard/FilterUserEntriesBrowser.php
+++ app/Livewire/Dashboard/FilterUserEntriesBrowser.php
@@ -2,12 +2,25 @@
 
 namespace App\Livewire\Dashboard;
 
+use App\Models\Franchise;
+use App\Models\UserEntry;
 use Livewire\Component;
 
 class FilterUserEntriesBrowser extends Component
 {
+    public ?Franchise $franchise = null;
+
+    public function getRandom()
+    {
+        $user = auth()->user();
+        $userEntry = UserEntry::inRandomOrder()->where('user_id', $user->id)->where('rating', null)->first();
+        $this->franchise = $userEntry->entry->franchise;
+    }
+
     public function render()
     {
-        return view('livewire.dashboard.filter-user-entries-browser');
+        return view('livewire.dashboard.filter-user-entries-browser', [
+            'franchise' => $this->franchise
+        ]);
     }
 }
--- app/Models/Franchise.php
+++ app/Models/Franchise.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
@@ -19,13 +20,13 @@ class Franchise extends Model
         'category_id'
     ];
 
-    public function category(): HasOne
+    public function category(): BelongsTo
     {
-        return $this->hasOne(Category::class);
+        return $this->belongsTo(Category::class);
     }
 
     public function entries(): HasMany
     {
         return $this->hasMany(Entry::class);
-    } 
+    }
 }
