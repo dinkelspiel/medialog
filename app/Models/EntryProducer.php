f6ec9a204e50a9ab584c58f2ad10b2895d76b793
keiidev <mail@keii.dev>
2023-10-08 20:10:28 +0200

    Updated filter feeling lucky screen

---
+++ app/Models/EntryProducer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class EntryProducer extends Model
+{
+    use HasFactory;
+    
+    public function entry(): BelongsTo
+    {
+        return $this->belongsTo(Entry::class);
+    }
+
+    public function person(): BelongsTo
+    {
+        return $this->belongsTo(Person::class);
+    }
+}
