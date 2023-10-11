d9bdf87bb67783751a60843c30935a60902e73f4
keii <mail@keii.dev>
2023-10-11 12:23:23 +0200

    WIP studio_id to entry_studios migration

---
+++ database/factories/EntryStudioFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Entry;
+use App\Models\Studio;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\EntryStudio>
+ */
+class EntryStudioFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'entry_id' => fake()->randomElement(Entry::pluck('id')->toArray()),
+            'studio_id' => fake()->randomElement(Studio::pluck('id')->toArray()),
+        ];
+    }
+}
