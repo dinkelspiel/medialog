7ddb86f672089ce2f3bb8c241df7f7aa724664cc
keiidev <mail@keii.dev>
2023-10-15 14:09:49 +0200

    Filter options for admin panel

--- app/Livewire/Admin/Entries.php
+++ app/Livewire/Admin/Entries.php
@@ -6,6 +6,22 @@ use Livewire\Component;
 
 class Entries extends Component
 {
+    public bool $show0Entries = false;
+    public bool $show0Creators = false;
+    public bool $show0Studios = false;
+
+    public function toggleShow0Entries() {
+        $this->show0Entries = !$this->show0Entries;
+    }
+
+    public function toggleShow0Creators() {
+        $this->show0Creators = !$this->show0Creators;
+    }
+
+    public function toggleShow0Studios() {
+        $this->show0Studios = !$this->show0Studios;
+    }
+
     public function render()
     {
         return view("livewire.admin.entries")->layout("layouts.app", [
