a2d9bacbd686158a7feb28c796db4e3822eab432
keii <mail@keii.dev>
2023-11-03 15:44:11 +0100

    Remove color_scheme column on user

--- database/migrations/2023_11_03_120039_create_color_schemes_table.php
+++ database/migrations/2023_11_03_120039_create_color_schemes_table.php
@@ -28,6 +28,10 @@ return new class extends Migration
             $table->char('text_gray', 7)->default('#A3A3A3');
             $table->timestamps();
         });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('color_scheme');
+        });
     }
 
     /**
@@ -36,5 +40,11 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('color_schemes');
+
+        Schema::table("users", function (Blueprint $table) {
+            $table
+                ->enum("color_scheme", ["auto", "dark", "light"])
+                ->default("auto");
+        });
     }
 };
