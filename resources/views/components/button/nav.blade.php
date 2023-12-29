d1910c17b210e8593b9678897a58d5b302c753cb
keii <mail@keii.dev>
2023-12-29 18:59:16 +0100

    Move sidebar to top when in mobile view (#16)

--- resources/views/components/button/nav.blade.php
+++ resources/views/components/button/nav.blade.php
@@ -1,3 +1,8 @@
-<x-button.nav.base class="c-hover-bg-secondary-hover">
-    {{ $slot }}
-</x-button.nav.base>
+<div class="flex flex-row lg:flex-col gap-3 lg:gap-1 text-base lg:text-xs items-center rounded-full">
+    <x-button.nav.base class="c-hover-bg-secondary-hover">
+        {{ $slot }}
+    </x-button.nav.base>
+    <div>
+        {{ $label ?? 'Label' }}
+    </div>
+</div>
--- resources/views/components/button/nav/selected.blade.php
+++ resources/views/components/button/nav/selected.blade.php
@@ -1,6 +1,13 @@
-<x-button.nav.base {{ $attributes->merge(['class' => 'border-2 c-border-outline relative c-hover-bg-secondary-hover']) }}>
-    <div class="c-bg-secondary w-1 h-[14px] rounded-full -left-0.5 absolute ring-2 c-ring-background">
+<div
+    class="flex flex-row lg:flex-col gap-3 lg:gap-1 text-base lg:text-xs items-center rounded-full c-shadow-card c-bg-card c-lg:shadow-none c-lg:bg-background">
+    <x-button.nav.base
+        {{ $attributes->merge(['class' => 'border-2 c-border-outline relative c-hover-bg-secondary-hover']) }}>
+        <div class="c-bg-secondary w-1 h-[14px] rounded-full -left-0.5 absolute ring-2 c-ring-background">
 
+        </div>
+        {{ $slot }}
+    </x-button.nav.base>
+    <div>
+        {{ $label ?? 'Label' }}
     </div>
-    {{ $slot }}
-</x-button.nav.base>
+</div>
