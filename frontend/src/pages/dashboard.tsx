f25ce966bfed31f132d9d5f18162cbd97dd53914
keiidev <mail@keii.dev>
2024-02-18 11:38:57 +0100

    Turn off scrollable grid on mobile

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -215,7 +215,7 @@ export default function Home() {
         <div className="grid grid-cols-1 grid-rows-[72px,1fr] lg:grid-cols-[256px,1fr] lg:grid-rows-1">
           <Sidebar user={user} />
           <div className="grid grid-cols-[1fr,max-content]">
-            <div className="scrollable-grid-item no-scrollbar flex flex-row gap-4 px-8 py-6">
+            <div className="lg:scrollable-grid-item no-scrollbar flex flex-row gap-4 px-8 py-6">
               <div className="w-full">
                 <div className="flex w-full flex-col gap-4  pb-4 lg:border-b lg:border-slate-200">
                   <div className="flex flex-row ">
