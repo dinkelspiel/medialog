ea870e1c02f271fc777abd9692126487efbab6ef
keii <mail@keii.dev>
2024-02-27 19:12:47 +0100

    Sidebar on mobile

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 
-export default function AppLayout({
+export default function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
   children,
