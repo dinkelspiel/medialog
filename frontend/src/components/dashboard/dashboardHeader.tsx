633356e388618cd62042e4e50ab5b547bfb156e7
keiidev <mail@keii.dev>
2024-03-10 16:24:37 +0100

    Filters to dialog

--- frontend/src/components/dashboard/dashboardHeader.tsx
+++ frontend/src/components/dashboard/dashboardHeader.tsx
@@ -22,6 +22,7 @@ import {
 import { cn } from "@/lib/utils";
 import { UserEntryStatusArray } from "@/interfaces/userEntryStatus";
 import { capitalizeFirst } from "@/lib/capitalizeFirst";
+import { Dialog, DialogContent } from "../ui/dialog";
 
 interface DashboardHeaderProps {
   sortBy: SortByType;
@@ -118,188 +119,194 @@ const DashboardHeader = ({
           </Button>
         </div>
       </Header>
-      {showFilters && (
-        <div className="flex flex-col gap-4 border-b border-b-slate-200 pb-4">
-          <div className="grid grid-cols-2 gap-4">
-            <div className="flex flex-col gap-2">
-              <Label>Title</Label>
-              <Input
-                onChange={(e) =>
-                  setFilter({ ...filter, title: e.target.value })
-                }
-                placeholder="Name"
-              />
+      <Dialog open={showFilters} onOpenChange={setShowFilters}>
+        <DialogContent>
+          <div className="flex flex-col gap-4 pb-4">
+            <div className="grid grid-cols-2 gap-4">
+              <div className="flex flex-col gap-2">
+                <Label>Title</Label>
+                <Input
+                  onChange={(e) =>
+                    setFilter({ ...filter, title: e.target.value })
+                  }
+                  placeholder="Name"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <Label>Status</Label>
+                <Popover
+                  open={filterStatusOpen}
+                  onOpenChange={setFilterStatusOpen}
+                >
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant="outline"
+                      role="combobox"
+                      className="w-full justify-between"
+                    >
+                      {filter.status
+                        ? filter.status !== "dnf"
+                          ? capitalizeFirst(filter.status)
+                          : "Did not finish"
+                        : "Select status..."}
+                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-full p-0">
+                    <Command>
+                      <CommandGroup>
+                        {UserEntryStatusArray.map((status) => (
+                          <CommandItem
+                            key={status}
+                            value={status}
+                            onSelect={(_) => {
+                              setFilter({
+                                ...filter,
+                                status:
+                                  status === filter.status ? undefined : status,
+                              });
+                              setFilterStatusOpen(false);
+                            }}
+                          >
+                            <Check
+                              className={cn(
+                                "mr-2 h-4 w-4",
+                                filter.status === status
+                                  ? "opacity-100"
+                                  : "opacity-0",
+                              )}
+                            />
+                            {status !== "dnf"
+                              ? capitalizeFirst(status)
+                              : "Did not finish"}
+                          </CommandItem>
+                        ))}
+                      </CommandGroup>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+              </div>
             </div>
-            <div className="flex flex-col gap-2">
-              <Label>Status</Label>
-              <Popover
-                open={filterStatusOpen}
-                onOpenChange={setFilterStatusOpen}
-              >
-                <PopoverTrigger asChild>
-                  <Button
-                    variant="outline"
-                    role="combobox"
-                    className="w-full justify-between"
-                  >
-                    {filter.status
-                      ? filter.status !== "dnf"
-                        ? capitalizeFirst(filter.status)
-                        : "Did not finish"
-                      : "Select status..."}
-                    <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-full p-0">
-                  <Command>
-                    <CommandGroup>
-                      {UserEntryStatusArray.map((status) => (
-                        <CommandItem
-                          key={status}
-                          value={status}
-                          onSelect={(_) => {
-                            setFilter({
-                              ...filter,
-                              status:
-                                status === filter.status ? undefined : status,
-                            });
-                            setFilterStatusOpen(false);
-                          }}
-                        >
-                          <Check
-                            className={cn(
-                              "mr-2 h-4 w-4",
-                              filter.status === status
-                                ? "opacity-100"
-                                : "opacity-0",
-                            )}
-                          />
-                          {status !== "dnf"
-                            ? capitalizeFirst(status)
-                            : "Did not finish"}
-                        </CommandItem>
-                      ))}
-                    </CommandGroup>
-                  </Command>
-                </PopoverContent>
-              </Popover>
+            <div className="grid grid-cols-2 gap-4">
+              <div className="flex flex-col gap-2">
+                <Label>Directors/Writers</Label>
+                <Popover
+                  open={filterPeopleOpen}
+                  onOpenChange={setFilterPeopleOpen}
+                >
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant="outline"
+                      role="combobox"
+                      className="w-full justify-between"
+                    >
+                      {filter.creator
+                        ? people.find(
+                            (person) => person.name === filter.creator,
+                          )?.name
+                        : "Select person..."}
+                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-full p-0">
+                    <Command>
+                      <CommandInput placeholder="Search people..." />
+                      <CommandEmpty>No people found.</CommandEmpty>
+                      <CommandGroup>
+                        {people.map((person) => (
+                          <CommandItem
+                            key={person.name}
+                            value={person.name}
+                            onSelect={(_) => {
+                              setFilter({
+                                ...filter,
+                                creator:
+                                  person.name === filter.creator
+                                    ? undefined
+                                    : person.name,
+                              });
+                              setFilterPeopleOpen(false);
+                            }}
+                          >
+                            <Check
+                              className={cn(
+                                "mr-2 h-4 w-4",
+                                filter.creator === person.name
+                                  ? "opacity-100"
+                                  : "opacity-0",
+                              )}
+                            />
+                            {person.name}
+                          </CommandItem>
+                        ))}
+                      </CommandGroup>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+              </div>
+              <div className="flex flex-col gap-2">
+                <Label>Studios</Label>
+                <Popover
+                  open={filterStudiosOpen}
+                  onOpenChange={setFilterStudiosOpen}
+                >
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant="outline"
+                      role="combobox"
+                      className="w-full justify-between"
+                    >
+                      {filter.studio
+                        ? studios.find(
+                            (studio) => studio.name === filter.studio,
+                          )?.name
+                        : "Select studio..."}
+                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-full p-0">
+                    <Command>
+                      <CommandInput placeholder="Search studios..." />
+                      <CommandEmpty>No studios found.</CommandEmpty>
+                      <CommandGroup>
+                        {studios.map((studio) => (
+                          <CommandItem
+                            key={studio.name}
+                            value={studio.name}
+                            onSelect={(_) => {
+                              setFilter({
+                                ...filter,
+                                studio:
+                                  studio.name === filter.studio
+                                    ? undefined
+                                    : studio.name,
+                              });
+                              setFilterStudiosOpen(false);
+                            }}
+                          >
+                            <Check
+                              className={cn(
+                                "mr-2 h-4 w-4",
+                                filter.studio === studio.name
+                                  ? "opacity-100"
+                                  : "opacity-0",
+                              )}
+                            />
+                            {studio.name}
+                          </CommandItem>
+                        ))}
+                      </CommandGroup>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+              </div>
             </div>
+            {!isDesktop && (
+              <SortByMobile setSortBy={setSortBy} sortBy={sortBy} />
+            )}
           </div>
-          <div className="grid grid-cols-2 gap-4">
-            <div className="flex flex-col gap-2">
-              <Label>Directors/Writers</Label>
-              <Popover
-                open={filterPeopleOpen}
-                onOpenChange={setFilterPeopleOpen}
-              >
-                <PopoverTrigger asChild>
-                  <Button
-                    variant="outline"
-                    role="combobox"
-                    className="w-full justify-between"
-                  >
-                    {filter.creator
-                      ? people.find((person) => person.name === filter.creator)
-                          ?.name
-                      : "Select person..."}
-                    <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-full p-0">
-                  <Command>
-                    <CommandInput placeholder="Search people..." />
-                    <CommandEmpty>No people found.</CommandEmpty>
-                    <CommandGroup>
-                      {people.map((person) => (
-                        <CommandItem
-                          key={person.name}
-                          value={person.name}
-                          onSelect={(_) => {
-                            setFilter({
-                              ...filter,
-                              creator:
-                                person.name === filter.creator
-                                  ? undefined
-                                  : person.name,
-                            });
-                            setFilterPeopleOpen(false);
-                          }}
-                        >
-                          <Check
-                            className={cn(
-                              "mr-2 h-4 w-4",
-                              filter.creator === person.name
-                                ? "opacity-100"
-                                : "opacity-0",
-                            )}
-                          />
-                          {person.name}
-                        </CommandItem>
-                      ))}
-                    </CommandGroup>
-                  </Command>
-                </PopoverContent>
-              </Popover>
-            </div>
-            <div className="flex flex-col gap-2">
-              <Label>Studios</Label>
-              <Popover
-                open={filterStudiosOpen}
-                onOpenChange={setFilterStudiosOpen}
-              >
-                <PopoverTrigger asChild>
-                  <Button
-                    variant="outline"
-                    role="combobox"
-                    className="w-full justify-between"
-                  >
-                    {filter.studio
-                      ? studios.find((studio) => studio.name === filter.studio)
-                          ?.name
-                      : "Select studio..."}
-                    <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-full p-0">
-                  <Command>
-                    <CommandInput placeholder="Search studios..." />
-                    <CommandEmpty>No studios found.</CommandEmpty>
-                    <CommandGroup>
-                      {studios.map((studio) => (
-                        <CommandItem
-                          key={studio.name}
-                          value={studio.name}
-                          onSelect={(_) => {
-                            setFilter({
-                              ...filter,
-                              studio:
-                                studio.name === filter.studio
-                                  ? undefined
-                                  : studio.name,
-                            });
-                            setFilterStudiosOpen(false);
-                          }}
-                        >
-                          <Check
-                            className={cn(
-                              "mr-2 h-4 w-4",
-                              filter.studio === studio.name
-                                ? "opacity-100"
-                                : "opacity-0",
-                            )}
-                          />
-                          {studio.name}
-                        </CommandItem>
-                      ))}
-                    </CommandGroup>
-                  </Command>
-                </PopoverContent>
-              </Popover>
-            </div>
-          </div>
-          {!isDesktop && <SortByMobile setSortBy={setSortBy} sortBy={sortBy} />}
-        </div>
-      )}
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
