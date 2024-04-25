b87bce6842a3900196e621577766c4a8a7d610a4
keiidev <mail@keii.dev>
2024-04-25 16:40:09 +0200

    feat: update text on homepage

--- app/(guest)/page.tsx
+++ app/(guest)/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Page = () => {
-  return <div className="font-bold text-xl">Test</div>;
+  return <div className="font-bold text-xl">W.I.P Medialog Homepage</div>;
 };
 
 export default Page;
