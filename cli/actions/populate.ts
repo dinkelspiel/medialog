a5ff1461c0469b3cb250fe3b434d70c585235d57
keiidev <mail@keii.dev>
2024-04-29 18:35:13 +0200

    fix: problem with fetching wikidata in cli

--- cli/actions/populate.ts
+++ cli/actions/populate.ts
@@ -1,24 +1,6 @@
 import prisma from '../../server/db';
 
 export const populate = async () => {
-  class SPARQLQueryDispatcher {
-    private endpoint: string;
-
-    constructor(endpoint: string) {
-      this.endpoint = endpoint;
-    }
-
-    query(sparqlQuery: string): Promise<any> {
-      const fullUrl: string =
-        this.endpoint + '?query=' + encodeURIComponent(sparqlQuery);
-      const headers: { [key: string]: string } = {
-        Accept: 'application/sparql-results+json',
-      };
-
-      return fetch(fullUrl, { headers }).then((body: Response) => body.json());
-    }
-  }
-
   const endpointUrl: string = 'https://query.wikidata.org/sparql';
   const sparqlQuery: string = `SELECT ?countryLabel ?iso_code ?languageCode ?languageLabel
       WHERE 
@@ -29,51 +11,52 @@ export const populate = async () => {
         SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
       }`;
 
-  const queryDispatcher: SPARQLQueryDispatcher = new SPARQLQueryDispatcher(
-    endpointUrl
-  );
+  const fullUrl: string =
+    endpointUrl + '?query=' + encodeURIComponent(sparqlQuery);
+  const headers: { [key: string]: string } = {
+    Accept: 'application/sparql-results+json',
+  };
 
-  queryDispatcher.query(sparqlQuery).then(async (dataString: any) => {
-    const data = dataString['results']['bindings'] as any[];
+  const dataString = await (await fetch(fullUrl, { headers })).json();
+  const data = dataString['results']['bindings'] as any[];
 
-    console.log('Populating countries and languages...');
+  console.log('Populating countries and languages...');
 
-    for (const entry of data) {
-      const language = await prisma.language.findFirst({
-        where: {
+  for (const entry of data) {
+    const language = await prisma.language.findFirst({
+      where: {
+        iso_639_1: entry.languageCode.value,
+      },
+    });
+
+    if (language === null) {
+      await prisma.language.create({
+        data: {
+          name: entry.languageLabel.value,
           iso_639_1: entry.languageCode.value,
         },
       });
 
-      if (language === null) {
-        await prisma.language.create({
-          data: {
-            name: entry.languageLabel.value,
-            iso_639_1: entry.languageCode.value,
-          },
-        });
+      console.log(`${entry.languageLabel.value} ${entry.languageCode.value}`);
+    }
 
-        console.log(`${entry.languageLabel.value} ${entry.languageCode.value}`);
-      }
+    const country = await prisma.country.findFirst({
+      where: {
+        iso_3166_1: entry.iso_code.value,
+      },
+    });
 
-      const country = await prisma.country.findFirst({
-        where: {
+    if (country === null) {
+      await prisma.country.create({
+        data: {
+          name: entry.countryLabel.value,
           iso_3166_1: entry.iso_code.value,
         },
       });
 
-      if (country === null) {
-        await prisma.country.create({
-          data: {
-            name: entry.countryLabel.value,
-            iso_3166_1: entry.iso_code.value,
-          },
-        });
-
-        console.log(`${entry.countryLabel.value} ${entry.iso_code.value}`);
-      }
+      console.log(`${entry.countryLabel.value} ${entry.iso_code.value}`);
     }
-  });
+  }
 
   console.log('Done populating countries and languages!');
 };
