a5c22f41f8aa9bc7637f7f2180aa5fe9787315ff
dinkelspiel <mail@keii.dev>
2024-04-30 11:16:49 +0200

    feat: add iso_639_2 to language table

--- cli/actions/populate.ts
+++ cli/actions/populate.ts
@@ -1,24 +1,27 @@
 import { existsSync, fstat, readFileSync } from 'fs';
 import prisma from '../../server/db';
 
-export const populate = async (options: { file: string | undefined }) => {
+export const populateCountries = async (options: {
+  file: string | undefined;
+}) => {
   let dataString;
   if (!options.file) {
     const endpointUrl: string = 'https://query.wikidata.org/sparql';
-    const sparqlQuery: string = `SELECT ?countryLabel ?iso_code ?languageCode ?languageLabel
-        WHERE 
-        {
-          ?country wdt:P297 ?iso_code .
-          ?country wdt:P37 ?language.
-          ?language wdt:P424 ?languageCode.
-          SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
-        }`;
+    const sparqlQuery: string = `SELECT ?countryLabel ?isoCode
+    WHERE {
+      ?country wdt:P31 wd:Q3624078.
+      ?country wdt:P297 ?isoCode.
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+    }
+    `;
 
     const fullUrl: string =
       endpointUrl + '?query=' + encodeURIComponent(sparqlQuery);
     const headers: { [key: string]: string } = {
       Accept: 'application/sparql-results+json',
     };
+
+    console.log(`Fetching: ${fullUrl}`);
 
     dataString = await (await fetch(fullUrl, { headers })).json();
   } else {
@@ -34,26 +37,9 @@ export const populate = async (options: { file: string | undefined }) => {
   console.log('Populating countries and languages...');
 
   for (const entry of data) {
-    const language = await prisma.language.findFirst({
-      where: {
-        iso_639_1: entry.languageCode.value,
-      },
-    });
-
-    if (language === null) {
-      await prisma.language.create({
-        data: {
-          name: entry.languageLabel.value,
-          iso_639_1: entry.languageCode.value,
-        },
-      });
-
-      console.log(`${entry.languageLabel.value} ${entry.languageCode.value}`);
-    }
-
     const country = await prisma.country.findFirst({
       where: {
-        iso_3166_1: entry.iso_code.value,
+        iso_3166_1: entry.isoCode.value,
       },
     });
 
@@ -61,7 +47,7 @@ export const populate = async (options: { file: string | undefined }) => {
       await prisma.country.create({
         data: {
           name: entry.countryLabel.value,
-          iso_3166_1: entry.iso_code.value,
+          iso_3166_1: entry.isoCode.value,
         },
       });
 
@@ -70,4 +56,67 @@ export const populate = async (options: { file: string | undefined }) => {
   }
 
   console.log('Done populating countries and languages!');
+};
+
+export const populateLanguages = async (options: {
+  file: string | undefined;
+}) => {
+  let dataString;
+  if (!options.file) {
+    const endpointUrl: string = 'https://query.wikidata.org/sparql';
+    const sparqlQuery: string = `SELECT ?languageLabel ?iso_639_1 ?iso_639_2
+    WHERE {
+      ?language wdt:P31 wd:Q34770.
+      OPTIONAL { ?language wdt:P218 ?iso_639_1. }
+      OPTIONAL { ?language wdt:P219 ?iso_639_2. }
+      FILTER((BOUND(?iso6391) || BOUND(?iso_639_2)))
+      ?language rdfs:label ?englishName.
+      FILTER(LANG(?englishName) = "en")
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+    }`;
+
+    const fullUrl: string =
+      endpointUrl + '?query=' + encodeURIComponent(sparqlQuery);
+    const headers: { [key: string]: string } = {
+      Accept: 'application/sparql-results+json',
+    };
+
+    console.log(`Fetching: ${fullUrl}`);
+
+    dataString = await (await fetch(fullUrl, { headers })).json();
+  } else {
+    if (!existsSync(options.file)) {
+      console.error("File doesn't exist");
+      return;
+    }
+
+    dataString = JSON.parse(readFileSync(options.file, 'utf8'));
+  }
+  const data = dataString['results']['bindings'] as any[];
+
+  console.log('Populating languages...');
+
+  for (const entry of data) {
+    const language = await prisma.language.findFirst({
+      where: {
+        iso_639_2: entry.iso_639_2.value,
+      },
+    });
+
+    if (language === null) {
+      await prisma.language.create({
+        data: {
+          name: entry.languageLabel.value,
+          iso_639_1: entry.iso_639_1 ? entry.iso_639_1.value : undefined,
+          iso_639_2: entry.iso_639_2.value,
+        },
+      });
+
+      console.log(
+        `${entry.languageLabel.value} ${entry.iso_639_2.value} ${entry.iso_639_1 ? entry.iso_639_1.value : undefined}`
+      );
+    }
+  }
+
+  console.log('Done populating languages!');
 };
