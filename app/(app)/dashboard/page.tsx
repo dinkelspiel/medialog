ec96dba64f5c2cba1e1e16c37eeabec284afb6d3
keiidev <mail@keii.dev>
2024-05-11 12:15:59 +0200

    feat: move authorization to individual pages in app so unauthenticated users can view profile

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,9 +1,14 @@
 import { validateSessionToken } from '@/server/auth/validateSession';
 import prisma from '@/server/db';
 import Dashboard from './client';
+import { redirect } from 'next/navigation';
 
 const Page = async () => {
   const user = await validateSessionToken();
+
+  if (!user) {
+    return redirect('/auth/login');
+  }
 
   const userEntries = await prisma.userEntry.findMany({
     where: {
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import '@/styles/globals.css';
 import BaseLayout from '@/components/layouts/base';
 
 export default function RootLayout({
-  // Layouts must accept a children prop.
-  // This will be populated with nested layouts or pages
   children,
 }: {
   children: React.ReactNode;
