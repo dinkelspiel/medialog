a63745d3aea8915c143a2d6430beabe2c2a20005
dinkelspiel <mail@keii.dev>
2024-06-26 12:07:38 +0200

    fix: remove space from commit in sidebar

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -40,8 +40,7 @@ const Layout = async ({ children }: { children: ReactNode }) => {
           <SidebarFooter>
             <div className="text-sm font-normal text-neutral-400">
               {process.env.GIT_COMMIT
-                ? `${process.env.GIT_COMMIT}@
-              ${process.env.GIT_BRANCH}`
+                ? `${process.env.GIT_COMMIT}@${process.env.GIT_BRANCH}`
                 : 'dev'}
             </div>
             <AddLog>
