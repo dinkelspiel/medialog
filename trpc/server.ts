8c8be635fd8caefc2ff1b9821a2fce92baa29e52
keiidev <mail@keii.dev>
2025-05-08 00:18:35 +0200

    fix trpc errors

--- trpc/server.ts
+++ trpc/server.ts
@@ -1,12 +1,12 @@
-import "server-only";
+import 'server-only';
 
-import { createHydrationHelpers } from "@trpc/react-query/rsc";
-import { headers } from "next/headers";
-import { cache } from "react";
+import { createHydrationHelpers } from '@trpc/react-query/rsc';
+import { headers } from 'next/headers';
+import { cache } from 'react';
 
-import { createCaller, type AppRouter } from "~/server/api/root";
-import { createTRPCContext } from "~/server/api/trpc";
-import { createQueryClient } from "./query-client";
+import { createCaller, type AppRouter } from '@/server/api/root';
+import { createTRPCContext } from '@/server/api/trpc';
+import { createQueryClient } from './query-client';
 
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
@@ -14,7 +14,7 @@ import { createQueryClient } from "./query-client";
  */
 const createContext = cache(async () => {
   const heads = new Headers(await headers());
-  heads.set("x-trpc-source", "rsc");
+  heads.set('x-trpc-source', 'rsc');
 
   return createTRPCContext({
     headers: heads,
