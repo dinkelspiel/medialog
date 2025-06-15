bce127d1ec66d38e654fffe8bba88b5a5f9e65cb
dinkelspiel <mail@keii.dev>
2025-06-15 13:40:58 +0200

    fix: create indexes before deleting all documents

--- app/api/migration/search/route.ts
+++ app/api/migration/search/route.ts
@@ -11,13 +11,13 @@ export const GET = async () => {
   console.log('Start');
   const entries = await prisma.entry.findMany();
 
+  console.log('Indexes');
+  await createIndexes();
   console.log('Delete...');
   await deleteAllDocuments();
-  await createIndexes();
 
   for (const entry of entries) {
     console.log(entry.id);
-    console.log('Add Entry');
     await addMeilisearchEntryByEntryId(entry.id);
   }
 };
