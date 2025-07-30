f15b398e8fa5ea4ff5ecc1331b904c3030f9e85f
dinkelspiel <mail@keii.dev>
2025-07-31 01:53:55 +0200

    chore: null coalece instead on env variables

--- server/api/routers/github.ts
+++ server/api/routers/github.ts
@@ -18,17 +18,19 @@ export const githubRouter = createTRPCRouter({
       const octokit = new Octokit({
         authStrategy: createAppAuth,
         auth: {
-          appId: process.env.GITHUB_APP_ID!,
-          installationId: process.env.GITHUB_INSTALLATION_ID!,
-          privateKey: process.env.GITHUB_PRIVATE_KEY!.split('####').join('\n'),
+          appId: process.env.GITHUB_APP_ID ?? '',
+          installationId: process.env.GITHUB_INSTALLATION_ID ?? '',
+          privateKey: (process.env.GITHUB_PRIVATE_KEY ?? '')
+            .split('####')
+            .join('\n'),
         },
       });
 
       const response = await octokit.request(
         'POST /repos/{owner}/{repo}/issues',
         {
-          owner: process.env.NEXT_PUBLIC_FEEDBACK_REPO_OWNER!,
-          repo: process.env.NEXT_PUBLIC_FEEDBACK_REPO!,
+          owner: process.env.NEXT_PUBLIC_FEEDBACK_REPO_OWNER ?? '',
+          repo: process.env.NEXT_PUBLIC_FEEDBACK_REPO ?? '',
           title: input.title,
           body: `user:${ctx.user.id}\nroute:${input.route}\n\n${input.body}`,
           labels: [input.label],
