68cdc18d0bde1d12e6c164d81093dba6ea71be29
keiidev <mail@keii.dev>
2025-08-04 09:40:41 +0200

    fix: problem with setting cookies in trpc auth router

--- trpc/react.tsx
+++ trpc/react.tsx
@@ -1,7 +1,12 @@
 'use client';
 
 import { QueryClientProvider, type QueryClient } from '@tanstack/react-query';
-import { loggerLink, unstable_httpBatchStreamLink } from '@trpc/client';
+import {
+  httpBatchLink,
+  httpBatchStreamLink,
+  loggerLink,
+  splitLink,
+} from '@trpc/client';
 import { createTRPCReact } from '@trpc/react-query';
 import { type inferRouterInputs, type inferRouterOutputs } from '@trpc/server';
 import { useState } from 'react';
@@ -47,14 +52,29 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
             process.env.NODE_ENV === 'development' ||
             (op.direction === 'down' && op.result instanceof Error),
         }),
-        unstable_httpBatchStreamLink({
-          transformer: SuperJSON,
-          url: getBaseUrl() + '/api/trpc',
-          headers: () => {
-            const headers = new Headers();
-            headers.set('x-trpc-source', 'nextjs-react');
-            return headers;
+        splitLink({
+          condition(op) {
+            console.log(op.path);
+            return op.path.startsWith('auth.');
           },
+          true: httpBatchLink({
+            transformer: SuperJSON,
+            url: getBaseUrl() + '/api/trpc',
+            headers() {
+              const headers = new Headers();
+              headers.set('x-trpc-source', 'react-no-stream');
+              return headers;
+            },
+          }),
+          false: httpBatchStreamLink({
+            transformer: SuperJSON,
+            url: getBaseUrl() + '/api/trpc',
+            headers() {
+              const headers = new Headers();
+              headers.set('x-trpc-source', 'react-stream');
+              return headers;
+            },
+          }),
         }),
       ],
     })
